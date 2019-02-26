f29ed69d985c2dd76974d235b112f7a6aafec51e
josephpan <m@hahack.com>
2019-02-26 11:58:03 +0800

    use brew as package installer.

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ install_homebrew(){
         test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
         test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
         echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+    fi
 }
 
 install_starter(){
