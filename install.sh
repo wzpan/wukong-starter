f91580a738eaaf243a1850acff965b7f64401724
josephpan <m@hahack.com>
2019-02-26 11:56:37 +0800

    use brew as package installer.

--- install.sh
+++ install.sh
@@ -2,85 +2,34 @@
 
 PWD=`pwd`
 
-install_starter(){
+install_homebrew(){
     os=$1
+    echo "正在为您安装homebrew"
     if [[ $os == "macos" ]]
-    then
-        echo "正在为您安装wukong-starter"
-        brew install portaudio sox ffmpeg swig
-        sudo pip3 install --upgrade pip
-        sudo pip3 install pyaudio
-        cd ~
-        wget http://hahack-1253537070.file.myqcloud.com/misc/snowboy.tar.bz2
-        tar -xvjf snowboy.tar.bz2
-        rm snowboy.tar.bz2
-        cd ~/snowboy/swig/Python3
-        make
-        sudo cp _snowboydetect.so ${PWD}/snowboy/
-        sudo rm -rf ~/snowboy
-        sudo chmod -R 777 ${PWD}
-    elif [[ $os == "linux" ]]
-    then
-        echo "正在为您安装wukong-starter"
-        sudo apt-get update && sudo apt-get upgrade -y
-        sudo apt-get install python3 python3-pip python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
-        sudo pip3 install --upgrade pip
-        sudo pip3 install pyaudio distribute
-        sudo pip3 install -r requirements.txt
-        sudo apt-get install python3-dev
-        sudo apt-get install python3-all-dev
-        sudo mkdir ~/install_temp && cd ~/install_temp
-        sudo wget http://hahack-1253537070.file.myqcloud.com/misc/swig-3.0.10.tar.gz
-        sudo tar xvf swig-3.0.10.tar.gz
-        cd swig-3.0.10
-        sudo apt-get install -y libpcre3 libpcre3-dev
-        ./configure --prefix=/usr --without-clisp --without-maximum-compile-warnings
-        sudo make && sudo make install
-        sudo install -v -m755 -d /usr/share/doc/swig-3.0.10
-        sudo cp -v -R Doc/* /usr/share/doc/swig-3.0.10
-        sudo apt-get install -y libatlas-base-dev
-        cd ~
-        wget http://hahack-1253537070.file.myqcloud.com/misc/snowboy.tar.bz2
-        tar -xvjf snowboy.tar.bz2
-        rm snowboy.tar.bz2
-        cd ~/snowboy/swig/Python3
-        sudo make
-        sudo cp _snowboydetect.so ${PWD}/snowboy/
-        sudo rm -rf ~/install_temp/ ~/snowboy/
-        sudo chmod -R 777 ${PWD}
-    elif [[ $os == "win10" ]]
-    then
-        echo "正在为您安装wukong-starter"
-        sudo add-apt-repository ppa:therealkenc/wsl-pulseaudio
-        sudo apt-get update && sudo apt-get upgrade -y
-        sudo apt-get install python3 python3-pip python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
-        sudo pip3 install --upgrade pip
-        sudo pip3 install pyaudio distribute
-        sudo pip3 install --upgrade --ignore-installed -r requirements.txt
-        sudo apt-get install python3-dev
-        sudo apt-get install python3-all-dev
-        sudo mkdir ~/install_temp && cd ~/install_temp
-        sudo wget http://hahack-1253537070.file.myqcloud.com/misc/swig-3.0.10.tar.gz
-        sudo tar xvf swig-3.0.10.tar.gz
-        cd swig-3.0.10
-        sudo apt-get install -y libpcre3 libpcre3-dev
-        ./configure --prefix=/usr --without-clisp --without-maximum-compile-warnings
-        sudo make && sudo make install
-        sudo install -v -m755 -d /usr/share/doc/swig-3.0.10
-        sudo cp -v -R Doc/* /usr/share/doc/swig-3.0.10
-        sudo apt-get install -y libatlas-base-dev
-        cd ~
-        wget http://hahack-1253537070.file.myqcloud.com/misc/snowboy.tar.bz2
-        tar -xvjf snowboy.tar.bz2
-        rm snowboy.tar.bz2
-        cd ~/snowboy/swig/Python3
-        sudo make
-        sudo cp _snowboydetect.so ${PWD}/snowboy/
-        sudo rm -rf ~/install_temp/ ~/snowboy/
-        sed -i '$a export PULSE_SERVER=tcp:localhost' ~/.bashrc
-        source ~/.bashrc
-        sudo chmod -R 777 ${PWD}
-    fi
+    then        
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+        test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+        test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+        echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+}
+
+install_starter(){
+    echo "正在为您安装wukong-starter"
+    brew install portaudio sox ffmpeg swig
+    sudo pip3 install --upgrade pip
+    sudo pip3 install pyaudio
+    cd ~
+    wget http://hahack-1253537070.file.myqcloud.com/misc/snowboy.tar.bz2
+    tar -xvjf snowboy.tar.bz2
+    rm snowboy.tar.bz2
+    cd ~/snowboy/swig/Python3
+    make
+    sudo cp _snowboydetect.so ${PWD}/snowboy/
+    sudo rm -rf ~/snowboy
+    sudo chmod -R 777 ${PWD}
 clear
 echo "wukong-starter 已安装完成!"
 
@@ -99,7 +48,8 @@ then
         echo "系统检测错误，已退出安装，请反馈给作者"
         exit
     else
-        install_starter "macos"
+        install_homebrew "macos"
+        install_starter
     fi
 elif [[ ${os_name[0]} == "Linux" && ${os_name[2]} =~ "Microsoft" ]]
 then   
@@ -109,7 +59,8 @@ then
         echo "系统检测错误，已退出安装，请反馈给作者"
         exit
     else
-        install_starter "win10"
+        install_homebrew "win10"
+        install_starter
     fi
 elif [[ ${os_name[0]} == "Linux" && ${os_name[1]} != "raspberrypi" ]]
 then
@@ -118,6 +69,7 @@ then
         echo "系统检测错误，已退出安装，请反馈给作者"
         exit
     else
-        install_starter "linux"
+        install_homebrew "linux"
+        install_starter
     fi
 fi
